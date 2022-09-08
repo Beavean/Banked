13f2add16fe0baf788ce0de6e36297dc57deef1e
Anthony Petrov <beavean@gmail.com>
2022-09-08 11:45:55 +0300

    AccountSummaryViewController and cells added

---
+++ Banked/AccountSummaryCell.swift
@@ -0,0 +1,55 @@
+//
+//  AccountSummaryCell.swift
+//  Banked
+//
+//  Created by Beavean on 07.09.2022.
+//
+
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    
+    let typeLabel = UILabel()
+    let underlineView = UIView()
+    
+    static let reuseID = "AccountSummaryCell"
+    static let rowHeight: CGFloat = 100
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension AccountSummaryCell {
+    
+    private func setup() {
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        typeLabel.adjustsFontForContentSizeCategory = true
+        typeLabel.text = "Account type"
+        contentView.addSubview(typeLabel)
+        
+        underlineView.translatesAutoresizingMaskIntoConstraints = false
+        underlineView.backgroundColor = appColor
+        contentView.addSubview(underlineView)
+        
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            underlineView.topAnchor.constraint(equalToSystemSpacingBelow: typeLabel.bottomAnchor, multiplier: 1),
+            underlineView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            underlineView.widthAnchor.constraint(equalToConstant: 60),
+            underlineView.heightAnchor.constraint(equalToConstant: 4),
+        ])
+    }
+}
--- Banked/AppDelegate.swift
+++ Banked/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
         mainViewController.selectedIndex = 1
         return true
     }
