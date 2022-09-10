3b23db53206bff630904b10cd28cc3a763477ee9
Anthony Petrov <beavean@gmail.com>
2022-09-10 08:36:51 +0300

    CurrencyFormatterTests added

---
+++ BankedUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,42 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankedUnitTests
+//
+//  Created by Beavean on 09.09.2022.
+//
+
+import Foundation
+import XCTest
+
+@testable import Banked
+
+class CurrencyFormatterTests: XCTestCase {
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testBreakZeroDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(0.00)
+        XCTAssertEqual(result.0, "0")
+        XCTAssertEqual(result.1, "00")
+    }
+    
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+}
